e10ac0ef6fe6d07cd67573095f939cb7dce5cf95
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-20 11:42:00 -0300

    feat: Item do pedido

---
+++ models/ItemPedido.cs
@@ -0,0 +1,9 @@
+﻿namespace PontoDeVenda.models
+{
+    public class ItemPedido
+    {
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; } = 0;
+        public decimal ValorTotal => Quantidade * Produto.Preco;
+    }
+}
